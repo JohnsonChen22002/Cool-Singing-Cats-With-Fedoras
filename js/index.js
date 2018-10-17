09b27c778e06c1ed2666f7a335aeb588d8d21de5
unknown <JohnsonChen12345@gmail.com>
2018-10-17 18:19:13 -0400

    HaveTestDataDisplayed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 // Write your JS here
 import test from './script2.js';
 
-console.log('test');
+// Declare your database
+var database = firebase.database();
 
-test();
+// Create your reference to your data. In this case we are rerencing the entire database at '/'
+var databaseRef = database.ref('/');
+
+//Listen to your database and get a snapshot at that reference
+databaseRef.once('value').then(function(snapshot) {
+  //Get the values from the snapshot of the data
+  const databaseValues = snapshot.val();
+  //Log all the data to the console
+  console.log(databaseValues);
+  // append the first name to the screen
+  $('body').append(databaseValues['Img'].Image1);
+});
