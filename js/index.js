afce33397ebd966024ed5f0a5720083b24058c11
Kevin <kevin.lima.code@gmail.com>
2018-11-14 18:07:41 -0500

    add and read from data base

--- js/index.js
+++ js/index.js
@@ -8,19 +8,27 @@ var database = firebase.database();
 var databaseRef = database.ref('/');
 
 //Listen to your database and get a snapshot at that reference
-databaseRef.once('value').then(function(snapshot) {
+databaseRef.on('value', function(snapshot) {
   //Get the values from the snapshot of the data
   const databaseValues = snapshot.val();
   //Log all the data to the console
   console.log(databaseValues);
+  for (var key in databaseValues) {
+    $('#messages').append(JSON.stringify(databaseValues[key]));
+  }
+  
   // append the first name to the screen
-  $('#messages').append("<img style='width:50%' src="+ databaseValues['Img'].Image1 +">")
+  // $('#messages').append("<img style='width:50%' src="+ databaseValues['Img'].Image1 +">")
 });
 
 
   $('#submit').click(function(){
-    var storage = $("#input").val();
-    $("#messages").append(storage);
+    var input = $("#input").val();
+    databaseRef.push({
+      "test": input
+    })
   })
   
   console.log('test');
+
+  
